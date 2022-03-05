d0748408fcedc277f2a4c819a5cc62bd94ca35e2
ab7z <abhiksi613@gmail.com>
2022-03-05 13:06:46 +0530

    last

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # What is this?
 
-This is a D3.JS Proejct where I make use of the d3-force package. So usually this package doesn't give the freedom for the users to locate their nodes, and even if we do manage to drag and change the positions, it resets back to its initial position on reload. So I've wrote some code to solve this problem :)
+This is a D3.JS Proejct where I make use of the d3-force package. So usually this package doesn't give the freedom for the users to locate their nodes, and even if we do manage to drag and change the positions, it resets back to its initial position on reload. So I've written some code to solve this problem :)
+
+
+# Tech Stack used
+1. PHP
+2. HTML/CSS/JS
+3. MySQL
 
 
 If you face any issues with this project, email me at abhinavcv007@gmail.com
---
+++ box.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2022 at 01:46 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `d3-research`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `box`
+--
+
+CREATE TABLE `box` (
+  `id` bigint(20) NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `vx` double NOT NULL,
+  `vy` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `box`
+--
+ALTER TABLE `box`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
